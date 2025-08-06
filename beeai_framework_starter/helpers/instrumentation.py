0903b2a5a50608618cf356983378841bbd92cd99
Tomáš Dvořák <toomas2d@gmail.com>
2025-08-06 18:51:07 +0200

    feat: update beeai-framework (#42)

--- beeai_framework_starter/helpers/instrumentation.py
+++ beeai_framework_starter/helpers/instrumentation.py
@@ -11,9 +11,6 @@ def setup_observability(endpoint: str = "http://localhost:6006/v1/traces") -> No
     Sets up OpenTelemetry with OTLP HTTP exporter and instruments the beeai framework.
     """
 
-    # Disable retries
-    OTLPSpanExporter._MAX_RETRY_TIMEOUT = 1
-
     resource = Resource(attributes={})
     tracer_provider = trace_sdk.TracerProvider(resource=resource)
     tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
